dadbba7ae1c696211c28ef0afb4195a8504162d3
tanlearner123 <tanlearner123@github.com>
2026-03-30 18:29:52 +0800

    feat(05): implement Event Entry Contracts

--- src/dm_bot/adventures/__init__.py
+++ src/dm_bot/adventures/__init__.py
@@ -1,4 +1,24 @@
 from dm_bot.adventures.loader import load_adventure
 from dm_bot.adventures.models import AdventurePackage, AdventureScene
+from dm_bot.adventures.action_intent import (
+    ActionIntent,
+    Entity,
+    IntentParseResult,
+    ValidationResult,
+    ClarificationReaction,
+)
+from dm_bot.adventures.intent_parser import IntentParser
+from dm_bot.adventures.intent_validator import IntentValidator
 
-__all__ = ["AdventurePackage", "AdventureScene", "load_adventure"]
+__all__ = [
+    "AdventurePackage",
+    "AdventureScene",
+    "load_adventure",
+    "ActionIntent",
+    "Entity",
+    "IntentParseResult",
+    "ValidationResult",
+    "ClarificationReaction",
+    "IntentParser",
+    "IntentValidator",
+]
